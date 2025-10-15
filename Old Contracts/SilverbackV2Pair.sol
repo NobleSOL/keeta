c1f6af658432c2c9ff9cd9fded6ccf4932a15821
Noble SOL <155856038+NobleSOL@users.noreply.github.com>
2025-10-15 10:10:47 -0700

    Add SilverbackV2Pair contract implementation

---
+++ Old Contracts/SilverbackV2Pair.sol
@@ -0,0 +1,196 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./interfaces.sol";
+
+contract SilverbackV2Pair {
+    using { _safeTransfer as safeTransfer } for address;
+
+    string public constant name = "Silverback V2 LP";
+    string public constant symbol = "SB-V2-LP";
+    uint8 public constant decimals = 18;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    address public factory;
+    address public token0;
+    address public token1;
+
+    uint112 private reserve0;
+    uint112 private reserve1;
+    uint32  private blockTimestampLast;
+
+    uint public price0CumulativeLast;
+    uint public price1CumulativeLast;
+    uint public kLast;
+
+    uint private unlocked = 1;
+    modifier lock() {
+        require(unlocked == 1, "LOCKED");
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to);
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    constructor() {
+        factory = msg.sender;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool s, bytes memory d) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
+        require(s && (d.length == 0 || abi.decode(d, (bool))), "TRANSFER_FAILED");
+    }
+
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, "FORBIDDEN");
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function getReserves() public view returns (uint112, uint112, uint32) {
+        return (reserve0, reserve1, blockTimestampLast);
+    }
+
+    function _mint(address to, uint value) private {
+        totalSupply += value;
+        balanceOf[to] += value;
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint value) private {
+        balanceOf[from] -= value;
+        totalSupply -= value;
+        emit Transfer(from, address(0), value);
+    }
+
+    function approve(address spender, uint value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) external returns (bool) {
+        uint allowed = allowance[from][msg.sender];
+        if (allowed != type(uint).max) allowance[from][msg.sender] = allowed - value;
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint value) private {
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+    }
+
+    function _update(uint balance0, uint balance1) private {
+        require(balance0 <= type(uint112).max && balance1 <= type(uint112).max, "OVERFLOW");
+        uint32 blockTimestamp = uint32(block.timestamp);
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow desired
+        if (timeElapsed > 0 && reserve0 != 0 && reserve1 != 0) {
+            price0CumulativeLast += uint(reserve1) * 1e18 / uint(reserve0) * timeElapsed;
+            price1CumulativeLast += uint(reserve0) * 1e18 / uint(reserve1) * timeElapsed;
+        }
+        reserve0 = uint112(balance0);
+        reserve1 = uint112(balance1);
+        blockTimestampLast = blockTimestamp;
+        emit Sync(reserve0, reserve1);
+    }
+
+    uint private constant MINIMUM_LIQUIDITY = 1000;
+
+    function mint(address to) external lock returns (uint liquidity) {
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+        uint amount0 = balance0 - _reserve0;
+        uint amount1 = balance1 - _reserve1;
+
+        if (totalSupply == 0) {
+            liquidity = _sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY); // lock first MINIMUM_LIQUIDITY
+        } else {
+            liquidity = _min(amount0 * totalSupply / _reserve0, amount1 * totalSupply / _reserve1);
+        }
+        require(liquidity > 0, "INSUFFICIENT_LIQUIDITY_MINTED");
+        _mint(to, liquidity);
+        _update(balance0, balance1);
+        kLast = uint(reserve0) * uint(reserve1);
+        emit Mint(msg.sender, amount0, amount1);
+    }
+
+    function burn(address to) external lock returns (uint amount0, uint amount1) {
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+        uint liquidity = balanceOf[address(this)];
+
+        amount0 = liquidity * balance0 / totalSupply;
+        amount1 = liquidity * balance1 / totalSupply;
+        require(amount0 > 0 && amount1 > 0, "INSUFFICIENT_LIQUIDITY_BURNED");
+        _burn(address(this), liquidity);
+        token0.safeTransfer(to, amount0);
+        token1.safeTransfer(to, amount1);
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+        _update(balance0, balance1);
+        kLast = uint(reserve0) * uint(reserve1);
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function swap(uint amount0Out, uint amount1Out, address to) external lock {
+        require(amount0Out > 0 || amount1Out > 0, "INSUFFICIENT_OUTPUT_AMOUNT");
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, "INSUFFICIENT_LIQUIDITY");
+
+        if (amount0Out > 0) token0.safeTransfer(to, amount0Out);
+        if (amount1Out > 0) token1.safeTransfer(to, amount1Out);
+
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint amount0In = balance0 + amount0Out > _reserve0 ? balance0 + amount0Out - _reserve0 : 0;
+        uint amount1In = balance1 + amount1Out > _reserve1 ? balance1 + amount1Out - _reserve1 : 0;
+        require(amount0In > 0 || amount1In > 0, "INSUFFICIENT_INPUT_AMOUNT");
+
+        // 0.3% fee to LPs
+        uint balance0Adjusted = balance0 * 1000 - amount0In * 3;
+        uint balance1Adjusted = balance1 * 1000 - amount1In * 3;
+        require(balance0Adjusted * balance1Adjusted >= uint(_reserve0) * uint(_reserve1) * 1_000_000, "K");
+
+        _update(balance0, balance1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+
+    function skim(address to) external lock {
+        token0.safeTransfer(to, IERC20(token0).balanceOf(address(this)) - reserve0);
+        token1.safeTransfer(to, IERC20(token1).balanceOf(address(this)) - reserve1);
+    }
+
+    function sync() external lock {
+        _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)));
+    }
+
+    function _min(uint x, uint y) private pure returns (uint z) { z = x < y ? x : y; }
+    function _sqrt(uint y) private pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) { z = x; x = (y / x + x) / 2; }
+        } else if (y != 0) { z = 1; }
+    }
+}
